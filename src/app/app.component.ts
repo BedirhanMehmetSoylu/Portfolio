d5d68cb15f5c0b8a931bdcc9d3b02cfcce06e18f
BedirhanMehmetSoylu <soylu.bedirhanmehmet@icloud.com>
2025-04-27 21:34:08 +0200

    did the structure of the template for the review container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ReviewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
