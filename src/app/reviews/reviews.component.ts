2a91bcaedb23bee135af52992129c38a8586c3cb
BedirhanMehmetSoylu <soylu.bedirhanmehmet@icloud.com>
2025-05-25 02:28:25 +0200

    added the translater for the reviews

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,29 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss'
 })
 export class ReviewsComponent {
   reviews = [
     {
-      text: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+      text: 'REVIEWS.FIRST',
       name: "V. Schuster - Team Partner",
       image: "reviewer.png",
       style: 'position: absolute; width: 260%; height: 120%; object-fit: cover; transform: rotate(90deg) translateX(20%);'
     },
     {
-      text: "Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a a significant contribution to us achieving our goals.",
+      text: "REVIEWS.SECOND",
       name: "E. Eichinger - Team Partner",
       image: "reviewer_2.jpg",
       style: 'position: absolute; width: 160%; height: 210%; object-fit: cover; transform: translateX(-3%) translateY(15%);'
     },
     {
-      text: "It was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm, Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.",
+      text: "REVIEWS.THIRD",
       name: "I. Nuber - Frontend Engineer",
       image: "reviewer_3.jpg",
       style: 'position: absolute; width: 190%; height: 290%; object-fit: cover; transform: translateX(7%) translateY(30%);'
