f1e93bc20440c1879b89df9d1ef49c6f2b807cc1
BedirhanMehmetSoylu <soylu.bedirhanmehmet@icloud.com>
2025-06-03 18:27:42 +0200

    added the json for the translater in privacy policy and responsiveness for it

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule, NavbarComponent, FooterComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss']
 })
