5b34303f9a5a945c632bdd69901b6e0f56e909ee
BedirhanMehmetSoylu <soylu.bedirhanmehmet@icloud.com>
2025-06-10 20:30:03 +0200

    added style to the images for reviewer to get all in the dom, to show the correct reviewer with display none or block

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -15,19 +15,16 @@ export class ReviewsComponent {
       text: 'REVIEWS.FIRST',
       name: "L. Eckert - Team Partner",
       image: "reviewer.png",
-      style: 'position: absolute; width: 120%; height: 200%; object-fit: cover; transform: translateX(-2%) translateY(15%);;'
     },
     {
       text: "REVIEWS.SECOND",
       name: "A. Hess - Team Partner",
       image: "reviewer_2.jpg",
-      style: 'position: absolute; width: 160%; height: 210%; object-fit: cover; transform: translateX(-3%) translateY(15%);'
     },
     {
       text: "REVIEWS.THIRD",
       name: "M. Tosyali - Team Partner",
       image: "reviewer_3.jpg",
-      style: 'position: absolute; width: 190%; height: 290%; object-fit: cover; transform: translateX(7%) translateY(30%);'
     }
   ];
 
